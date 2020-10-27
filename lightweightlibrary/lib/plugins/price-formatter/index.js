a38a02d0a7b1b66c710ff6d329505907f7cd39d2
Alexandr Nevyantsev <nevyantsev.alexandr@gmail.com>
2020-10-28 00:04:18 +0300

    replace stringFormatter with priceFormatter

---
+++ lightweightlibrary/lib/plugins/price-formatter/index.js
@@ -0,0 +1,10 @@
+window.priceFormatter = (formatterParams) => {
+    return (...params) => {
+        console.log(params, formatterParams)
+        var template = formatterParams.template
+        params.forEach((param, index) => {
+            template = template.replace(new RegExp("\\{" + index + "\\}", "g"), param)
+        })
+        return template
+    }
+}
