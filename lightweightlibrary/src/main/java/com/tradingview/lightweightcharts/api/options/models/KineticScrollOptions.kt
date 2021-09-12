4e31d2fef7d06f819ac4a0fecca5a836674fb6ac
makedonsky94 <nevyantsev.alexandr@gmail.com>
2021-09-13 00:38:44 +0300

    Add support for kinetic scroll

---
+++ lightweightlibrary/src/main/java/com/tradingview/lightweightcharts/api/options/models/KineticScrollOptions.kt
@@ -0,0 +1,20 @@
+package com.tradingview.lightweightcharts.api.options.models
+
+/**
+ * You can disable or enable kinetic scroll via mouse or via touch gestures separately
+ */
+data class KineticScrollOptions(
+    /**
+     * If true, kinetic scroll is enabled via mouse
+     */
+    var touch: Boolean? = null,
+
+    /**
+     * If true, kinetic scroll is enabled via touch gestures
+     */
+    var mouse: Boolean? = null,
+)
+
+inline fun kineticScrollOptions(init: KineticScrollOptions.() -> Unit): KineticScrollOptions {
+    return KineticScrollOptions().apply(init)
+}
