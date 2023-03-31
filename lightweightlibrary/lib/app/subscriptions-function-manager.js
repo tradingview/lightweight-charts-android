4341f2e225441e3d4af04add51d6a7ca58c7c704
Andrey Kuzubov <akuzubov@tradingview.com>
2023-03-31 18:58:54 +0400

    crosshair and type mapping

--- lightweightlibrary/lib/app/subscriptions-function-manager.js
+++ lightweightlibrary/lib/app/subscriptions-function-manager.js
@@ -1,4 +1,4 @@
-import { logger } from './logger.js'
+import {logger} from './logger.js'
 
 export default class SubscriptionsFunctionManager {
 
@@ -16,7 +16,7 @@ export default class SubscriptionsFunctionManager {
                     const subscription = (params) => {
                         let customSeries = []
                         params.seriesPrices.forEach((value, key, map) => {
-                            customSeries.push({ id: this.seriesFunctionManager.getSeriesId(key, input), prices: value })
+                            customSeries.push({id: this.seriesFunctionManager.getSeriesId(key, input), prices: value})
                         })
                         params.seriesPrices = customSeries
                         callback(params)
@@ -45,11 +45,17 @@ export default class SubscriptionsFunctionManager {
                 try {
                     console.error(this.seriesFunctionManager)
                     const subscription = (params) => {
+                        params.sourceEvent = this.selectProps(
+                            "clientX", "clientY", "pageX", "pageY", "screenX", "screenY",
+                            "localX", "localY", "ctrlKey", "altKey", "shiftKey", "metaKey"
+                        )(params.sourceEvent)
+
                         let customSeries = []
-                        params.seriesPrices.forEach((value, key, map) => {
-                            customSeries.push({ id: this.seriesFunctionManager.getSeriesId(key, input), prices: value })
+                        params.seriesData.forEach((value, key, map) => {
+                            customSeries.push({id: this.seriesFunctionManager.getSeriesId(key, input), prices: value})
                         })
-                        params.seriesPrices = customSeries
+                        params.seriesData = customSeries
+
                         callback(params)
                     }
                     chart.subscribeCrosshairMove(subscription)
@@ -70,5 +76,18 @@ export default class SubscriptionsFunctionManager {
             }
         )
     }
+
+
+    selectProps(...props) {
+        return function (obj) {
+            const newObj = {};
+            props.forEach(name => {
+                newObj[name] = obj[name];
+            });
+
+            return newObj;
+        }
+    }
+
 
 }
--- lightweightlibrary/src/main/java/com/tradingview/lightweightcharts/api/series/models/MouseEventParams.kt
+++ lightweightlibrary/src/main/java/com/tradingview/lightweightcharts/api/series/models/MouseEventParams.kt
@@ -1,7 +1,29 @@
 package com.tradingview.lightweightcharts.api.series.models
 
 data class MouseEventParams(
+    /**
+     * The X coordinate of the mouse pointer in local (DOM content) coordinates.
+     */
     val time: Time? = null,
+    /**
+     * Logical index
+     */
+    val logical: Int? = null,
+
+    /**
+     * Location of the event in the chart.
+     */
     val point: Point? = null,
-    val seriesPrices: Array<BarPrices>? = null
-)
+
+    /**
+     * Data of all series at the location of the event in the chart.
+     */
+    val seriesPrices: List<BarPrices>? = null,
+
+
+    /**
+     * The underlying source mouse or touch event data, if available
+     */
+    val sourceEvent: TouchMouseEventData? = null,
+
+    )
