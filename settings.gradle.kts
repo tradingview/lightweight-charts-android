2b9c8b501688b1b462ef3edfd33bca523cc07114
Andrey Kuzubov <akuzubov@tradingview.com>
2025-10-31 17:45:30 +0100

    prepare to migrate to toml scheme

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,37 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
+}
+
+
+buildCache {
+    local {
+        isPush = true
+        directory = File(rootDir, ".gradle/build-cache")
+        removeUnusedEntriesAfterDays = 1
+    }
+}
+
 rootProject.name = "LightweightCharts"
+
 
 include(
     ":app",
